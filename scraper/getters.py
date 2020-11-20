0d339e183c4a70d21ad88c9d88e92fa0439b5491
Christian Melot <christianmelot1989@gmail.com>
2020-11-20 07:59:54 +0100

    getters.py

--- scraper/getters.py
+++ scraper/getters.py
@@ -1,23 +1,26 @@
 from requests import get
 from datetime import datetime
 
-def get_appartement_urls() -> list:
-    '''function to get urls from the 'a-vendre' section'''
+
+def get_urls() -> list:
+    '''function to get urls from all section'''
     detail_urls = []
-    for i in range(300):
-        url = f"https://www.immoweb.be/fr/search-results/appartement/a-vendre?countries=BE&page={i}&orderBy=relevance"#génère une nouvelle url =égale une nouvelle page de recherche
-        response = get(url)
-        source = None
-        if response.status_code == 200:
-            source = response.json()
+    search_values = ["appartement","house"]
+    for value in search_values:
+        for i in range(1):#page results
+            url = f"https://www.immoweb.be/fr/search-results/{value}/a-vendre?countries=BE&page={i}&orderBy=relevance"#génère une nouvelle url =égale une nouvelle page de recherche
+            response = get(url)
+            source = None
+            if response.status_code == 200:
+                source = response.json()
 
-        if source:
-            results = source["results"]
-            for j in range(len(results)):
-                id_ = results[j]["id"]
-                locality = results[j]["property"]["location"]["locality"]
-                postal_code = results[j]["property"]["location"]["postalCode"]
+            if source:
+                results = source["results"]
+                for j in range(len(results)):
+                    id_ = results[j]["id"]
+                    locality = results[j]["property"]["location"]["locality"]
+                    postal_code = results[j]["property"]["location"]["postalCode"]
 
-                detail_urls.append(
-                    f"https://www.immoweb.be/fr/annonce/appartement/a-vendre/{locality}/{postal_code}/{id_}")#récupère toutes les liens sur la page de recherche
+                    detail_urls.append(
+                        f"https://www.immoweb.be/fr/annonce/appartement/a-vendre/{locality}/{postal_code}/{id_}")#récupère toutes les liens sur la page de recherche
     return detail_urls
